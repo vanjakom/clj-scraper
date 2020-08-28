39065dd57b65671d032c4d77b90fe45b4fa3e14c
Vanja Komadinovic <vanjakom@gmail.com>
2020-08-28 13:48:52 +0200

    retrieve using dynamic configuration

--- src/clj_scraper/scrapers/org/wikidata.clj
+++ src/clj_scraper/scrapers/org/wikidata.clj
@@ -5,9 +5,6 @@
    [clj-common.json :as json]
    [clj-scraper.scrapers.retrieve :as retrieve]))
 
-(def configuration
-  {:keep-for (* 30 24 60 60 1000)})
-
 (defn entity [id]
   (second
    (first
@@ -15,9 +12,9 @@
      (json/read-keyworded
       (retrieve/retrieve
        (assoc
-        configuration
+        retrieve/*configuration*
         :url
-        (str "http://www.wikidata.org/wiki/Special:EntityData/" id ".json"))))))))
+        (str "https://www.wikidata.org/wiki/Special:EntityData/" id ".json"))))))))
 
 (defn wikipedia-title [language title]
   (second
@@ -26,7 +23,7 @@
      (json/read-keyworded
       (retrieve/retrieve
        (assoc
-        configuration
+        retrieve/*configuration*
         :url
         (str
          "https://www.wikidata.org/w/api.php?action=wbgetentities&sites="
--- src/clj_scraper/scrapers/retrieve.clj
+++ src/clj_scraper/scrapers/retrieve.clj
@@ -9,7 +9,6 @@
   (:require [clj-common.cache :as cache])
   (:require [clj-scraper.env :as env]))
 
-
 (defn fs-serialize [entry]
   (json/serialize
     (assoc
@@ -31,52 +30,60 @@
                :value-serialize-fn fs-serialize
                :value-deserialize-fn fs-deserialize}))
 
+;; deprecated
 (def default-configuration
   {
-    :keep-for (* 24 60 60)})
+   :keep-for (* 24 60 60)
+   :cache-fn cache-fn})
 
+(def ^:dynamic *configuration* default-configuration)
+
+;; todo
+;; fix must be added in each scraper to support dynamic configuration
+;; extract url as only parameter to retrieve and always use dynamic configuration
 
 (defn retrieve
   "Should either retrieve InputStream to page or download it
   Note: currently returning only InputStream but headers and status are kept
   also in cache"
-  [configuration]
-  (let [url (:url configuration)
-        timestamp (time/timestamp-second)
-        keep-for (or (:keep-for configuration) 0)]
-    (let [result-from-cache (cache-fn url)]
-      (if
-        (or
-          (nil? result-from-cache)
-          (and
-            (not (= keep-for -1))
-            (> timestamp (+ (get result-from-cache :timestamp 0) keep-for))))
-        (let [result (http/get-raw-as-stream url)]
-          (if (and
+  ([]
+   (retrieve *configuration*))
+  ([configuration]
+   (let [url (:url configuration)
+         timestamp (time/timestamp-second)
+         keep-for (or (:keep-for configuration) 0)
+         ;; safety for raw configurations which do not have cache-fn
+         cache-fn (or (:cache-fn configuration) cache-fn)]
+     (let [result-from-cache (cache-fn url)]
+       (if
+           (or
+            (nil? result-from-cache)
+            (and
+             (not (= keep-for -1))
+             (> timestamp (+ (get result-from-cache :timestamp 0) keep-for))))
+         (let [result (http/get-raw-as-stream url)]
+           (if (and
                 (= (:status result) 200)
                 (some? (:body result)))
-            (let [result-to-cache {
+             (let [result-to-cache {
                                     :url url
                                     :timestamp timestamp
                                     :data (io/input-stream->bytes (:body result))}]
-              (logging/report {
+               (logging/report {
                                 :url url
                                 :timestamp timestamp
                                 :retireve :download-ok})
-              (cache-fn url result-to-cache)
-              (io/bytes->input-stream (:data result-to-cache)))
-            (do
-              (logging/report {
+               (cache-fn url result-to-cache)
+               (io/bytes->input-stream (:data result-to-cache)))
+             (do
+               (logging/report {
                                 :url url
                                 :timestamp timestamp
                                 :retireve :download-fail})
-              nil)))
-        (do
-          (logging/report {
+               nil)))
+         (do
+           (logging/report {
                             :url url
                             :timestamp timestamp
                             :retireve :cache})
-          (io/bytes->input-stream (:data result-from-cache)))))))
-
-
-
+           (io/bytes->input-stream (:data result-from-cache))))))))
