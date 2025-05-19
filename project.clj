69a01b5a51c766142b6a33a15c15455e60f78cbc
Vanja Komadinovic <vanjakom@gmail.com>
2025-05-19 18:47:22 +0200

    0.1.0 using release versions

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject com.mungolab/clj-scraper "0.1.0-SNAPSHOT"
+(defproject
+  com.mungolab/clj-scraper
+  "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                  [com.mungolab/clj-common "0.3.0-SNAPSHOT"]
+                  [com.mungolab/clj-common "0.3.2"]
                   [enlive "1.1.6"]])
